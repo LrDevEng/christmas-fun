1a6723547a90b412696147eb437d4a3a2d23f138
LrDevEng <lr.deveng@gmail.com>
2024-12-12 17:51:15 +0100

    Update about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,39 @@
+import Link from 'next/link';
+
 export const metadata = {
   title: 'About',
   description: 'Christmas fun about page.',
 };
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="text-center flex flex-col justify-between min-h-[95vh]">
+      <div>
+        <div className="text-xl mt-8">
+          Designed & Developed with 💚 by LrDevEng
+        </div>
+        <div className="text-xl mt-8">
+          Follow my work on{' '}
+          <Link
+            className="underline"
+            href="https://github.com/LrDevEng"
+            target="_blank"
+          >
+            GitHub
+          </Link>
+          .
+        </div>
+      </div>
+      <div className="text-sm my-8">
+        Background image designed by{' '}
+        <Link
+          className="underline"
+          href="https://www.freepik.com/"
+          target="_blank"
+        >
+          Freepik.
+        </Link>
+      </div>
+    </div>
+  );
 }
